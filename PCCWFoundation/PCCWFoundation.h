4f0896288ff0448acf7037b69150bfc01c84855c
lizhihui0215 <lizhihui0215@gmail.com>
2016-12-13 17:07:20 +0800

    refine project

---
+++ PCCWFoundation/PCCWFoundation.h
@@ -0,0 +1,19 @@
+//
+//  PCCWFoundation.h
+//  PCCWFoundation
+//
+//  Created by 李智慧 on 13/12/2016.
+//  Copyright © 2016 PCCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PCCWFoundation.
+FOUNDATION_EXPORT double PCCWFoundationVersionNumber;
+
+//! Project version string for PCCWFoundation.
+FOUNDATION_EXPORT const unsigned char PCCWFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PCCWFoundation/PublicHeader.h>
+
+
