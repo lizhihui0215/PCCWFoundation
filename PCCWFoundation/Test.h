a6d17fea7b1227bd87ce87e9540c3ff51e8bf59a
lizhihui0215 <lizhihui0215@gmail.com>
2016-12-13 16:56:35 +0800

    init

---
+++ PCCWFoundation/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  PCCWFoundation
+//
+//  Created by 李智慧 on 13/12/2016.
+//  Copyright © 2016 PCCW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ PCCWFoundation/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  PCCWFoundation
+//
+//  Created by 李智慧 on 13/12/2016.
+//  Copyright © 2016 PCCW. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
