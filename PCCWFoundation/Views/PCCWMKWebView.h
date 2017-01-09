a96101ee3f55cdcac2ef0e7783a377278d62821a
lizhihui0215 <lizhihui0215@gmail.com>
2017-01-09 14:51:34 +0800

    fix bugs add Masonry

--- PCCWFoundation/Views/PCCWMKWebView.h
+++ PCCWFoundation/Views/PCCWMKWebView.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
-#import <WebViewJavascriptBridge/WebViewJavascriptBridge.h>
+#import <PCCWWebViewJavascriptBridge/PCCWWebViewJavascriptBridge.h>
 
 @interface PCCWMKWebView : UIView
 
